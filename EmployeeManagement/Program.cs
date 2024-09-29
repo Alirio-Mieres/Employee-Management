0d3d16777e1d78f2ae3fe9b3fb16fcd5ad7db01b
Alirio Mieres <mie21001@byui.edu>
2024-09-28 23:36:26 -0400

    Add examples for Structures and Union in Program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -30,6 +30,23 @@
                 Conditionals.PrintTernary();
                 Conditionals.PrintNullCoalescing();
                 Loops.PrintLoops();
+                // Structures
+                Point p = new Point(10, 20);
+                p.Display(); // Output: Point: (10, 20)
+                // Union Example
+                // Create an instance of the UnionExample structure
+                UnionExample u = new UnionExample();
+
+                // Assign an integer value to the IntValue field
+                u.IntValue = 42;
+
+                // Print the value of the FloatValue field interpreting the same memory space as a float
+                // The output will be a float value representing the bits of 42 as a float
+                Console.WriteLine(u.FloatValue); 
+
+                // Print the value of the ByteValue field interpreting the same memory space as a byte
+                // The output will be the value of the first byte of the binary representation of 42
+                Console.WriteLine(u.ByteValue);  
 
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();

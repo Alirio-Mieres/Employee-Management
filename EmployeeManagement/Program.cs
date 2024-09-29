1b64b3f112f4deb8480f82ad307bc1963f56da66
Alirio Mieres <mie21001@byui.edu>
2024-09-28 23:23:44 -0400

    Add Program.cs file to implement the main program logic for managing employees and displaying a menu.

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Menu options
+        string[] options = { "1- Run Employee Program", "2- Show the concepts code", "Exit" };
+        EmployeeApp EmployeeManagement = new EmployeeApp();
+
+        // Call the menu
+        while (true)
+        {
+            int selectedOption = Menu.ShowMenu(options);
+
+            // Check the selected option
+            if (options[selectedOption] == "Exit")
+            {
+                break; // Exit the program
+            }
+            else if (options[selectedOption] == "1- Run Employee Program")
+            {
+
+                EmployeeManagement.RunEmployeeProgram();
+            }
+            else if (options[selectedOption] == "2- Show the concepts code")
+            {
+                // Show the concepts code
+                DataTypes.PrintDataTypes();
+                Expressions.PrintExpressions();
+                Conditionals.PrintConditionals();
+                Conditionals.PrintTernary();
+                Conditionals.PrintNullCoalescing();
+                Loops.PrintLoops();
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"You selected: {options[selectedOption]}");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        // Final message
+        Console.Clear();
+        Console.WriteLine("Thanks for using the program!");
+    }
+}
